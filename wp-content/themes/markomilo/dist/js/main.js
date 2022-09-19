14c1520b54d8b62662703ae6c3ac3baa9cd8db68
Marko <marko.nikolas@marbleit.rs>
2022-09-19 17:44:09 +0200

    Fix work page scrolling feature

--- wp-content/themes/markomilo/dist/js/main.js
+++ wp-content/themes/markomilo/dist/js/main.js
@@ -1,1 +1,5 @@
-import InfiniteMenu from"./infiniteMenu.js";jQuery(".page-template-poetry").length&&new InfiniteMenu(document.querySelector("nav.main-navigation"));
+import InfiniteMenu from './infiniteMenu.js';
+
+if (jQuery('.page-template-poetry').length) {
+    new InfiniteMenu(document.querySelector('nav.main-navigation'));
+}
