cfc93e1434bc9823c698699ee9f4309b4aecb952
Marko <marko.nikolas@marbleit.rs>
2022-09-20 20:58:34 +0200

    Fix issues when navigatingpages

--- wp-content/themes/markomilo/poetry.php
+++ wp-content/themes/markomilo/poetry.php
@@ -12,26 +12,29 @@ namespace MarkoMilo;
 
 <?php get_header(); ?>
 
-	<nav class="main main-navigation" data-barba="container" data-barba-namespace="Works">
+	<main class="main" data-barba="container" data-barba-namespace="Works">
+		<section class="main-navigation">
+			<?php
 
-		<?php
+			$args = array(
+				'post_type'      => 'poem',
+				'posts_per_page' => -1,
+				'order'          => 'ASC',
+			);
 
-		$args = array(
-			'post_type'      => 'poem',
-			'posts_per_page' => -1,
-			'order'          => 'ASC',
-		);
+			$query = new \WP_Query( $args );
 
-		$query = new \WP_Query( $args );
+			if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) :
+					$query->the_post();
 
-		if ( $query->have_posts() ) :
-			while ( $query->have_posts() ) :
-				$query->the_post();
+					?>
 
-				?>
-				<div class="main__link"><a class="main__link-inner" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-			<?php endwhile; ?>
-		<?php endif; ?>
-	</nav>
+					<div class="main__link"><a class="main__link-inner" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</section>
+	</main>
 
 <?php get_footer(); ?>
