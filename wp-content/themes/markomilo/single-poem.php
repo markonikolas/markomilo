3d51d31e7a8e7e21da0fb89a8dd931a6f1eef097
Marko <marko.nikolas@marbleit.rs>
2022-10-14 09:43:35 +0200

    Update poem to show/hide purchase link & synopsis

--- wp-content/themes/markomilo/single-poem.php
+++ wp-content/themes/markomilo/single-poem.php
@@ -21,6 +21,7 @@ if ( ! $next_post ) {
 	$next_post = get_post( custom_posttype_get_adjacent_id( 'prev', 'poem', get_the_ID() ) );
 }
 
+$purchase_link = get_field( 'purchase_link' );
 ?>
 
 <?php get_header(); ?>
@@ -32,11 +33,15 @@ if ( ! $next_post ) {
 
 		<?php echo get_the_post_thumbnail(); ?>
 
+		<?php if ( get_field( 'show_synopsis' ) ) : ?>
+
 		<blockquote>
 			— <?php echo esc_html( get_field( 'quote_value' ) ); ?>
 		</blockquote>
 
 		<hr>
+
+		<?php endif; ?>
 
 		<?php the_content(); ?>
 
@@ -54,7 +59,13 @@ if ( ! $next_post ) {
 			<div>
 				<h3 class="post-meta__title"><?php esc_html_e( 'Printed book:', 'markomilo' ); ?></h3>
 
-				<p class="post-meta__text"><a class="post-meta__link underline" href="#"><?php esc_html_e( 'Purchase now', 'markomilo' ); ?></a></p>	
+				<?php if ( $purchase_link ) : ?>
+					<p class="post-meta__text">
+						<a class="post-meta__link underline" href="<?php echo esc_url( $purchase_link['url'] ); ?>">
+							<?php echo esc_attr( $purchase_link['title'] ); ?>
+						</a>
+					</p>	
+				<?php endif; ?>
 			</div>
 		</section>
    
